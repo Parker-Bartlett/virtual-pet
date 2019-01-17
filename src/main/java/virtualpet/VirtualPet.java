db06a7ccf20b54815dad35195bcfb7576720f244
Parker Bartlett <prbartlett1@gmail.com>
2019-01-17 14:37:34 -0500

    Test food and name

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,5 +1,18 @@
 package virtualpet;
 
 public class VirtualPet {
+
 	String name;
+
+	public VirtualPet(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHunger() {
+		return 10;
+	}
 }
