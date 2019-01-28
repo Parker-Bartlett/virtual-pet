06e9b48b72a47ade3a1b6ebde735e3ae6ca20292
Eythan Kramm <kramm.eythan@gmail.com>
2019-01-28 10:30:46 -0500

    Application update

--- src/main/java/virtualpet/Application.java
+++ src/main/java/virtualpet/Application.java
@@ -10,62 +10,85 @@ public class Application {
 		String name = input.nextLine();
 		VirtualPet pet = new VirtualPet(name);
 		System.out.println("Meet " + pet.getName() + "!");
-		
+
 		System.out.println("To take a tour press 1. \nTo get started press 2.");
 		String choice = input.nextLine();
 
-	
 		boolean foo = true;
 		while (foo) {
 			if (choice.equals("1")) {
 				String petAction;
-				
-				System.out.println("Welcome to the Shelter.  In order to keep your pets happy and healthy, you will need to care for your pet");
-				
+				pet.tick();
+
+				System.out.println(
+						"Welcome to the Shelter.  In order to keep your pets happy and healthy, you will need to care for your pet");
+
 				System.out.print("Your pet is hungry! Feed it by pressing 1.");
 				petAction = input.nextLine();
 				if (petAction.equals("1")) {
 					pet.feed();
-					}
+				}
 				System.out.print("Your pet is thursty! Hydrate it by pressing 2.");
 				petAction = input.nextLine();
 				if (petAction.equals("2")) {
 					pet.hydrate();
-					}
+				}
 				System.out.print("Your pet is board! Play with it by pressing 3.");
 				petAction = input.nextLine();
 				if (petAction.equals("3")) {
 					pet.play();
-					}
+				}
 				System.out.print("Your pet is dirty! Clean it by pressing 4.");
 				petAction = input.nextLine();
 				if (petAction.equals("4")) {
 					pet.clean();
-					}
-				
+				}
+
 				foo = false;
 				break;
-				
+
 			} else if (choice.equals("2")) {
 				foo = false;
 				break;
 			} else {
 				System.out.println("Please enter 1 or 2.");
-				choice = input.next();
+				choice = input.nextLine();
 			}
 		}
 
-
-		// Intro and tutorial
+		// Intro to Game
+		System.out.println("Congratulations you have a happy and healthy pet! Lets get started!");
 		System.out.println(pet.printStats());
 
-		// for (int i = 0; i < 11; i++) {
-		// pet.tick();
-		// System.out.println(pet.getBoredom() +" "+ pet.getHunger() + " " +
-		// pet.getThirst() + " " + pet.getCleanliness());
-		//
-		
+		// Menu
+		boolean isAlive = true;
+		while (isAlive) {
+			System.out.println("1. Feed \n2. Hydrate \n3. Play \n4. Clean \n5. Check Status \n6. Exit");
+			String menuChoice = input.nextLine();
+			pet.tick();
+			
+			switch (menuChoice) {
+			case "1":
+				pet.feed();
+				break;
+			case "2":
+				pet.hydrate();
+				break;
+			case "3":
+				pet.play();
+				break;
+			case "4":
+				pet.clean();
+				break;
+			case "5":
+				System.out.println(pet.printStats());
+				break;
+			case "6":
+				System.exit(0);
+				break;
+			}
+
+		}
 		input.close();
 	}
-
 }
